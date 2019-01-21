e94e4eba4928c1ed7eee8e1641a309db8ff103ac
vasam <venkatesh.vasam13@gmail.com>
2019-01-22 00:29:41 +0530

    Added Extra Info section in employee-details page

--- client/src/app/@application/employee-details/employee-details.component.ts
+++ client/src/app/@application/employee-details/employee-details.component.ts
@@ -21,7 +21,9 @@ import {
 export class EmployeeDetailsComponent implements OnInit, OnDestroy {
   employeeId: number;
   employeeStore: Store;
+  extraInfoStore: Store;
   employeeRow: Row;
+  extraInfoRows: Row[];
   employeeForm: FormGroup;
   maritalStatus = ['Single', 'Married', 'Not Mentioned'];
   yesOrNO = ['Y', 'N'];
@@ -57,44 +59,54 @@ export class EmployeeDetailsComponent implements OnInit, OnDestroy {
       'employees',
       []
     );
+    this.extraInfoStore = this.storeService.getInstance(
+      'usersExtraInfo',
+      'usersExtraInfo',
+      []
+    );
     this.employeeForm = this.formBuilder.group({
-      details: this.formBuilder.group({
-        employeeNo: new FormControl(),
-        firstName: new FormControl(),
-        middleName: new FormControl(),
-        lastName: new FormControl(),
-        displayName: new FormControl(),
-        fatherName: new FormControl(),
-        gender: new FormControl(),
-        dateOfBirth: new FormControl(),
-        maritalStatus: new FormControl(),
-        emailAddress: new FormControl(),
-        mobile: new FormControl(),
-        title: new FormControl(),
-        dateOfJoin: new FormControl(),
-        managerNo: new FormControl(),
-        managerName: new FormControl(),
-        leftOrg: new FormControl(),
-        resignSubmitDate: new FormControl(),
-        relieveDate: new FormControl(),
-        leavingDate: new FormControl(),
-        noticePeriod: new FormControl(),
-        isNoticeRequired: new FormControl(),
-        cofirmDate: new FormControl(),
-        status: new FormControl(),
-        leavingReason: new FormControl()
-      })
+      details: this.formBuilder.group(this.getDetailsFormGroup()),
+      extraInfo: this.formBuilder.array([])
     });
     if (this.employeeId > -1) {
       this.employeeStore.whereClause =
         'user_id = ? and (deleted = ? or deleted is null)';
       this.employeeStore.whereClauseParams = [this.employeeId, 'N'];
+      this.extraInfoStore.whereClause = 'user_id = ?';
+      this.extraInfoStore.whereClauseParams = [this.employeeId];
+
       this.employeeStore.query().then(response => {
         if (response.status === Status.SUCCESS && response.rows.length > 0) {
           this.employeeRow = response.rows[0];
           this.copyRowToForm('details', this.employeeRow);
         } else {
           this.throwError1(this.employeeId);
+        }
+      });
+      this.extraInfoStore.query().then(response => {
+        if (response.status === Status.SUCCESS && response.rows.length > 0) {
+          this.extraInfoRows = response.rows;
+          response.rows.forEach((row: Row, index) => {
+            const control = new FormGroup(this.getExtraInfoFormGroup());
+            (<FormArray>this.employeeForm.get('extraInfo')).push(control);
+          });
+
+          this.copyRowsToForm('extraInfo', this.extraInfoRows);
+        } else {
+          this.throwError1(this.employeeId);
+        }
+      });
+    }
+  }
+
+  copyRowsToForm(formGroup: string, rows: Row[]) {
+    const formArray = this.employeeForm.get(formGroup)['controls'] as FormArray;
+    for (let i = 0; i < formArray.length; i++) {
+      const cntl = formArray[i];
+      const row = rows[i];
+      Object.keys(row).forEach(key => {
+        if (cntl['controls'].hasOwnProperty(key)) {
+          cntl['controls'][key].setValue(row[key]);
         }
       });
     }
@@ -125,7 +137,8 @@ export class EmployeeDetailsComponent implements OnInit, OnDestroy {
 
   save = () => {
     console.log(this.employeeForm.value.details);
-  }
+    console.log(this.employeeForm.value.extraInfo);
+  };
 
   onNameChange = () => {
     const details = this.employeeForm.value.details;
@@ -143,9 +156,66 @@ export class EmployeeDetailsComponent implements OnInit, OnDestroy {
     }
     formGroup.controls['displayName'].setValue(name.trim());
     //  }
-  }
+  };
 
   ngOnDestroy() {
     this.employeeStore.destroy();
+    this.extraInfoStore.destroy();
+  }
+
+  getBorderStyleFix(index: number) {
+    const formArray = this.employeeForm.get('extraInfo')[
+      'controls'
+    ] as FormArray;
+    if (index < formArray.length) {
+      return 'border-bottom: 0px';
+    }
+  }
+
+  getExtraInfoFormGroup() {
+    return {
+      userId: new FormControl(),
+      employeeNo: new FormControl(),
+      key: new FormControl(),
+      value: new FormControl(),
+      effectiveFrom: new FormControl(),
+      createDate: new FormControl(),
+      createUserId: new FormControl(),
+      updateDate: new FormControl(),
+      updateUserId: new FormControl()
+    };
+  }
+  getDetailsFormGroup() {
+    return {
+      userId: new FormControl(),
+      employeeNo: new FormControl(),
+      firstName: new FormControl(),
+      middleName: new FormControl(),
+      lastName: new FormControl(),
+      displayName: new FormControl(),
+      fatherName: new FormControl(),
+      gender: new FormControl(),
+      dateOfBirth: new FormControl(),
+      maritalStatus: new FormControl(),
+      emailAddress: new FormControl(),
+      mobile: new FormControl(),
+      title: new FormControl(),
+      dateOfJoin: new FormControl(),
+      managerNo: new FormControl(),
+      managerName: new FormControl(),
+      leftOrg: new FormControl(),
+      resignSubmitDate: new FormControl(),
+      relieveDate: new FormControl(),
+      leavingDate: new FormControl(),
+      noticePeriod: new FormControl(),
+      isNoticeRequired: new FormControl(),
+      cofirmDate: new FormControl(),
+      status: new FormControl(),
+      leavingReason: new FormControl(),
+      createDate: new FormControl(),
+      createUserId: new FormControl(),
+      updateDate: new FormControl(),
+      updateUserId: new FormControl()
+    };
   }
 }
